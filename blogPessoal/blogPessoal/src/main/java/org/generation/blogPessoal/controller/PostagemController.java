df157a9bb7cb026ab03e4614345c4baffea6ec16
Clamant96 <kevin.lazzarotto@hotmail.com>
2021-03-18 11:33:56 -0300

    Implementacao de funcao de tratamento de dados. Foram incluidas dentro da classe Controller os metodos de POST, PUT e DELETE para podemos manipular nossa base de dados por meio de requisicoes de um usuario

--- blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
+++ blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/controller/PostagemController.java
@@ -5,10 +5,15 @@ import java.util.List;
 import org.generation.blogPessoal.model.Postagem;
 import org.generation.blogPessoal.repository.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -35,11 +40,34 @@ public class PostagemController {
 															
 	}
 	
+	// o @PathVariable serve para podermos capiturar um dado inserido dentor de nossa URI
 	@GetMapping("/titulo/{titulo}") // retorna uma pequisa de um titulo ou por partes das letras desse titulo
 	public ResponseEntity<List<Postagem>> GetByTitulo(@PathVariable String titulo) { // recebe como parametro letras de um titulo
 		
 		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo)); // por meio da query criada dentro do Repository, podemos utilizalo para 
 																						  //realizar nossa pesquisa por meio de letras ou pavras completas de titulos como se fosse o LIKE do SQL
+	}
+	
+	// insere na base de dados uma nova postagem, semque que for solicitado uma funcao do tipo Post, sera chamado essa funcao de insercao de dados
+	// @RequestBody server para podermos capiturar tudo que esta no corpo da requisicao ou no objeto Postagem
+	@PostMapping
+	public ResponseEntity<Postagem> post(@RequestBody Postagem postagem){
+		
+		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(postagem));
+	}
+	
+	// atualiza na base de dados uma postagem
+	@PutMapping
+	public ResponseEntity<Postagem> put(@RequestBody Postagem postagem){
+		
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(postagem));
+	}
+	
+	// deleta um determinado dado levando como paremetro seu id
+	@DeleteMapping("/{id}") // {id} <= interpolacao ou template literal
+	public void delete(@PathVariable long id) {
+		
+		repository.deleteById(id);
 	}
 
 }
